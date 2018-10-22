ee0d1ece780c747459918863c617ec7486f8cebb
temyers <tim.myerscough@mechanicalrock.io>
2018-10-22 17:41:03 +0800

    increase watch limit for user files - required for jest:watch

--- system/install.sh
+++ system/install.sh
@@ -1,4 +1,7 @@
 # Set lid switch to hybrid-sleep
-echo "HandleLidSwitch=hybrid-sleep" | sudo tee -a /etc/systemd/logind.conf
+sudo cp etc/systemd/* /etc/systemd/
 
 sudo apt-get install -y keepassx
+
+sudo cp etc/system.d/* /etc/system.d/
+sudo sysctl --system
